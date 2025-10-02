c6072a02784e41a1626e18bf8d65eeea0d94676b
lamar <setemikay@gmail.com>
2025-10-02 19:40:43 +0100

    eee

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 function addTask() {
-
     let input = document.getElementById("inputstring");
     if (input.value.trim() === "") return;
+
     let li = document.createElement("li");
     li.textContent = input.value;
-    li.onclick = () => li.classList.toggle("done"); // mark as done
+    li.onclick = () => li.classList.toggle("completed");
+
     document.getElementById("list").appendChild(li);
     input.value = "";
 }
+
+document.querySelector("button").addEventListener("click", addTask);
+
+document.getElementById("inputstring").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") addTask();
+});
